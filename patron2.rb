4d176194a545757399841d463d00745986a5edde
maria alvarez <mar.alvareg@gmail.com>
2020-08-21 16:10:11 -0400

    patron2

--- patron2.rb
+++ patron2.rb
@@ -1,16 +1,10 @@
 num = ARGV[0].to_i
-def patron(size)
-    cont=1
-    (size/2).times do |j|
-        if cont==1 
-           print '**' 
-           cont=cont+1
-        elsif cont==2
-            print '..'
-            cont=1
-        end
+num.times do |i|
+    if i%4==0 || i%4==1
+        print '*'
+    else
+        print '.'
     end
-    puts 
 end
+puts
 #solo funciona con numeros pares, pero el ejercio dice q funcione con 10 so....
-patron(num)
